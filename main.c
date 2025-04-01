ad9aa8ea6e904416a619d815459232c572e24d1e
akshat <d24126231@mytudublin.ie>
2025-04-01 22:47:52 +0100

    working

--- main.c
+++ main.c
@@ -12,66 +12,106 @@ struct part
     int date;
 };
 
-void generateRandomData(struct part *, struct part *, struct part *, struct part *);
-void merge(char *array[], int low, int mid, int high, struct part *parts) ;
-void mergeSort(char *array[], int low, int high, struct part *parts);
-int findWeight(char *id, struct part *parts );
+#define Morning 10
+#define Afternoon 10
+#define Evening 10
+#define Night 10
+
+void generateRandomData(struct part *, int);
+void merge(char *array[], int low, int mid, int high, struct part *parts, int) ;
+void mergeSort(char *array[], int low, int high, struct part *parts, int);
+int findWeight(char *id, struct part *parts, int );
 
 
 int main()
-{
-    // declaring different time parts
-    struct part morningParts[10], eveningParts[10], afternoonParts[10], nightParts[10];
-
-    //passing array to function
-    printf("\nGenerating Random Data...\n");
-
-    int startTime = clock();
-    generateRandomData(morningParts, eveningParts, afternoonParts, nightParts);
-    int endTime = clock();
-
-    printf("Random Data Generation Complete! time taken : %d Milliseconds\n\n" ,(endTime - startTime));
-
-    
-    //copying ids into an array
-    char *ids[10]; 
-    printf("Morning Batch Code: ");
-    for (int i = 0; i < 10; i++)
-    {
-        ids[i] = morningParts[i].batchCode;
-        printf("%s ", ids[i]);
-    }
-
-    printf("\nUnsorted Weights : ");
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", findWeight(morningParts[i].batchCode, morningParts));
-    }
-
-
-    mergeSort(ids, 0, 9, morningParts); 
-
-    printf("\n\nSorted Batch Code: ");
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%s ", ids[i]);
-    }
-    printf("\nSorted Weights : ");
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", findWeight(ids[i], morningParts));
-    }
-
-    printf("\n");
-}
-
-
-void generateRandomData( struct part *morningParts, struct part *eveningParts, struct part *afternoonParts, struct part *nightParts)
-{
+{   
     // seeding time
     srand(time(NULL));
+    
+    // declaring different time parts
+    struct part morningParts[Morning], eveningParts[Evening], afternoonParts[Afternoon], nightParts[Night];
+    struct part all[Morning + Afternoon + Evening + Night];
 
-    for (int i = 0; i < 10; i++)
+    //generating random data
+    generateRandomData(morningParts, Morning);
+    generateRandomData(afternoonParts, Afternoon);
+    generateRandomData(eveningParts, Evening);
+    generateRandomData(nightParts, Night);
+
+    // making one big id of everything ( works till here) + making a new big struct of all data
+    char *allids[Morning + Afternoon + Evening + Night];
+    for(int i = 0; i < Morning + Afternoon + Evening + Night; i++)
+    {
+        if (i < Morning)
+        {
+            allids[i] = morningParts[i].batchCode;
+            strcpy(all[i].batchCode,morningParts[i].batchCode);
+            all[i].weight = morningParts[i].weight;
+            all[i].line = morningParts[i].line;
+            all[i].targetEngineCode =  morningParts[i].targetEngineCode;
+        } 
+        else if (Morning <= i && i < (Afternoon + Morning))
+        {
+            allids[i] = afternoonParts[i - Morning].batchCode;
+            strcpy(all[i].batchCode,afternoonParts[i - Morning].batchCode);
+            all[i].weight = afternoonParts[i - Morning].weight;
+            all[i].line = afternoonParts[i - Morning].line;
+            all[i].targetEngineCode =  afternoonParts[i - Morning].targetEngineCode;
+        }
+        else if ((Afternoon + Morning) <= i && i < (Evening + Afternoon + Morning))
+        {
+            allids[i] = eveningParts[i - (Morning + Afternoon)].batchCode;
+            strcpy(all[i].batchCode,eveningParts[i - (Morning + Afternoon)].batchCode);
+            all[i].weight = eveningParts[i - (Morning + Afternoon)].weight;
+            all[i].line = eveningParts[i - (Morning + Afternoon)].line;
+            all[i].targetEngineCode =  eveningParts[i - (Morning + Afternoon)].targetEngineCode;
+        }
+        else if ((Evening + Afternoon + Morning) <= i && i < (Night + Morning+ Afternoon + Evening))
+        {
+            allids[i] = nightParts[i - (Morning + Afternoon + Evening)].batchCode;
+            strcpy(all[i].batchCode,nightParts[i - (Morning + Afternoon + Evening)].batchCode);
+            all[i].weight = nightParts[i - (Morning + Afternoon + Evening)].weight;
+            all[i].line = nightParts[i - (Morning + Afternoon + Evening)].line;
+            all[i].targetEngineCode =  nightParts[i - (Morning + Afternoon + Evening)].targetEngineCode;
+        }
+    }
+
+    
+
+    printf("All ids:  ");
+
+    for(int i = 0; i < Morning + Afternoon + Evening + Night; i++)
+    {
+        printf("%s ",all[i].batchCode);
+    }
+
+    printf("\nAll weight:  ");
+
+    for(int i = 0; i < Morning + Afternoon + Evening + Night; i++)
+    {
+        printf("%d ",all[i].weight);
+    }
+
+    printf("\nSorted Batch Code: ");
+    mergeSort(allids,0,Morning + Afternoon + Evening + Night - 1,all, Morning + Afternoon + Evening + Night);
+    for(int i = 0; i < Morning + Afternoon + Evening + Night; i++)
+    {
+        printf("%s ",allids[i]);
+    }
+
+    printf("\nSorted weight:  ");
+
+    for(int i = 0; i < Morning + Afternoon + Evening + Night; i++)
+    {
+        printf("%d ",findWeight(allids[i], all, Morning + Afternoon + Evening + Night));
+    }
+};
+
+void generateRandomData( struct part *Parts, int len)
+{
+    
+
+    for (int i = 0; i < len; i++)
     {   
         // generating numbers
 
@@ -81,63 +121,40 @@ void generateRandomData( struct part *morningParts, struct part *eveningParts, s
         int a = rand();
         
         //generating random weight
-        morningParts[i].weight = m % 10;
-        eveningParts[i].weight = e % 10;
-        nightParts[i].weight = n % 10;
-        afternoonParts[i].weight = a % 10;
+        Parts[i].weight = m % 10;
 
         //generatingrandomline
-        morningParts[i].line = m % 4 + 1;
-        eveningParts[i].line = n % 4 + 1;
-        afternoonParts[i].line = a % 4 + 1;
-        nightParts[i].line = n % 4 + 1;
+        Parts[i].line = m % 4 + 1;
+        
 
         //generating random batchCode
-        for (int k = 0; k < 10; k++)
-        {   
-            // generating character for ex, AB, FD
-            for ( int j = 0; j < 2; j++ ){
-                int w1 = rand();
-                morningParts[k].batchCode[j] = 'A' + (w1) % 25;
-
-                int j1 = rand();
-                nightParts[k].batchCode[j] = 'A' + (j1) % 25;
-
-                int k1 = rand();
-                eveningParts[k].batchCode[j] = 'A' + (k1) % 25;
-
-                int l1 = rand();
-                afternoonParts[k].batchCode[j] = 'A' + (l1) % 25;
-            }
-
-            //num elements of batchCode ex 11, 12, 41
-            for (int k = 2; k < 4 ; k++){
-                int w2 = rand();
-                morningParts[i].batchCode[k] = '0' + w2 % 10;
-                int i2 = rand();
-                nightParts[i].batchCode[k] = '0' + i2 % 10;
-                int k2 = rand();
-                afternoonParts[i].batchCode[k] = '0' + k2 % 10;
-                int l2 = rand();
-                eveningParts[i].batchCode[k] = '0' + l2 % 10;
-
-                morningParts[i].batchCode[4] = '\0';
-            }
-
-            // generating target engineCode
-            for (int t = 0; t < 4; t++)
-            {
-                morningParts[t].targetEngineCode = m * n;
-                eveningParts[t].targetEngineCode = n * e;
-                afternoonParts[t].targetEngineCode = m * e;
-                nightParts[t].targetEngineCode = a * e;
-            }
-
+         
+        // generating character for ex, AB, FD
+        for ( int j = 0; j < 2; j++ ){
+            int w1 = rand();
+            Parts[i].batchCode[j] = 'A' + (w1) % 25;
         }
+
+        //num elements of batchCode ex 11, 12, 41
+        for (int k = 2; k < 4 ; k++){
+            int w2 = rand();
+            Parts[i].batchCode[k] = '0' + w2 % 10;
+            Parts[i].batchCode[4] = '\0';
+        }
+
+
+        // generating target engineCode
+        for (int t = 0; t < 4; t++)
+        {
+            Parts[t].targetEngineCode = m * n;
+        }
+
+        
+        
     } 
 }
 
-void merge(char *array[], int low, int mid, int high, struct part *parts) 
+void merge(char *array[], int low, int mid, int high, struct part *parts, int len) 
 {
     int i, j, k;
     int leftSize = mid - low + 1;
@@ -160,7 +177,7 @@ void merge(char *array[], int low, int mid, int high, struct part *parts)
 
     
    while (i < leftSize && j < rightSize){
-    if (findWeight(left[i],parts) <= findWeight(right[j], parts)){
+    if (findWeight(left[i],parts,len) <= findWeight(right[j], parts,len)){
         array[k] = left[i];
         i++;
     } else{
@@ -183,21 +200,21 @@ void merge(char *array[], int low, int mid, int high, struct part *parts)
     }  
 }
 
-void mergeSort(char *array[], int low, int high, struct part *parts) 
+void mergeSort(char *array[], int low, int high, struct part *parts, int len) 
 {
     if (low < high) {
         int mid = low + (high - low) / 2;
-        mergeSort(array, low, mid, parts);
-        mergeSort(array, mid + 1, high, parts);
-        merge(array, low, mid, high, parts);
+        mergeSort(array, low, mid, parts, len);
+        mergeSort(array, mid + 1, high, parts, len);
+        merge(array, low, mid, high, parts, len);
     }
 }
 
 
 
-int findWeight(char *id, struct part *parts )
+int findWeight(char *id, struct part *parts, int len )
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < len; i++)
     {
         if((strcmp(parts[i].batchCode, id) == 0))
         {   
@@ -207,3 +224,4 @@ int findWeight(char *id, struct part *parts )
 
     return 0;
 }
+
